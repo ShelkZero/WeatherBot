e1de4b823f1e9d741187f8483776295a62059ecf
Anna Karban <annakant37@gmail.com>
2024-08-13 13:20:47 +0300

    Add first file

---
+++ Weather_Bot.py
@@ -0,0 +1,14 @@
+﻿import telebot
+
+API_TOKEN = '7093337586:AAFvNFOKuRAtpBiwiFPneDIEc4nYD5qnIZ8'
+
+bot = telebot.TeleBot(API_TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = telebot.types.ReplyKeyboardMarkup()
+    button = telebot.types.KeyboardButton('Открыть веб старницу', web_app=telebot.types.WebAppInfo(url='https://github.com/ShelkZero'))
+    markup.add(button)
+    bot.send_message(message.chat.id, 'Привет, мой друг!', reply_markup=markup)
+        
+bot.infinity_polling()
